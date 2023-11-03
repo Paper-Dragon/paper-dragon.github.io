13dc38b0e192ab639f9bd7b62fc7f116d312963f
Paper-Dragon <2678885646@qq.com>
2023-11-03 05:45:18 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@b45121f77052c16bedf80173b830f4ee153cfe75 🚀

---
+++ assets/ngx_stream_ssl_preread_module.html-8ade200e.js
@@ -0,0 +1,21 @@
+import{_ as a}from"./plugin-vue_export-helper-c27b6911.js";import{r as t,o,c as i,a as n,e as s,d as p,f as c}from"./app-eaf7338a.js";const l={},r=n("h1",{id:"ngx-stream-ssl-preread-module",tabindex:"-1"},[n("a",{class:"header-anchor",href:"#ngx-stream-ssl-preread-module","aria-hidden":"true"},"#"),s(" ngx_stream_ssl_preread_module")],-1),d={href:"https://docshome.gitbook.io/nginx-docs/he-xin-gong-neng/stream/ngx_stream_ssl_preread_module",target:"_blank",rel:"noopener noreferrer"},u=c(`<p>允许从clienthello中提取信息，而不会终止SSL/TLS,例如通过sni请求的服务器名称</p><div class="language-nginx line-numbers-mode" data-ext="nginx"><pre class="language-nginx"><code><span class="token directive"><span class="token keyword">map</span> <span class="token variable">$ssl_preread_server_name</span> <span class="token variable">$name</span></span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">backend.example.com</span>      backend</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">default</span>                  backend2</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token directive"><span class="token keyword">upstream</span> backend</span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">server</span> 192.168.0.1:12345</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">server</span> 192.168.0.2:12345</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token directive"><span class="token keyword">upstream</span> backend2</span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">server</span> 192.168.0.3:12345</span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">server</span> 192.168.0.4:12345</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token directive"><span class="token keyword">server</span></span> <span class="token punctuation">{</span>
+    <span class="token directive"><span class="token keyword">listen</span>      <span class="token number">12346</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">proxy_pass</span>  <span class="token variable">$name</span></span><span class="token punctuation">;</span>
+    <span class="token directive"><span class="token keyword">ssl_preread</span> <span class="token boolean">on</span></span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2);function k(v,m){const e=t("ExternalLinkIcon");return o(),i("div",null,[r,n("blockquote",null,[n("p",null,[n("a",d,[s("https://docshome.gitbook.io/nginx-docs/he-xin-gong-neng/stream/ngx_stream_ssl_preread_module"),p(e)])])]),u])}const g=a(l,[["render",k],["__file","ngx_stream_ssl_preread_module.html.vue"]]);export{g as default};
