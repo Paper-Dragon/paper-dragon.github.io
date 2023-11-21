575df1ea169fd2ccb24ee547a6c0e296bc8f26b2
Paper-Dragon <2678885646@qq.com>
2023-11-21 09:52:21 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@db8140287411212d41fd70489773d4cba025ed01 🚀

---
+++ assets/Nginx正向代理.html-k80W557X.js
@@ -0,0 +1,77 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,e}from"./app-FAsVQrIy.js";const i={},t=e(`<h1 id="nginx代理" tabindex="-1"><a class="header-anchor" href="#nginx代理" aria-hidden="true">#</a> nginx代理</h1><blockquote><p>nginx本身是不支持https协议请求转发，为了让nginx能达到这一效果需要借助第三方模块ngx_http_proxy_connect_module</p></blockquote><h2 id="配置" tabindex="-1"><a class="header-anchor" href="#配置" aria-hidden="true">#</a> 配置</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token comment">#user  nobody;</span>
+worker_processes  <span class="token number">1</span><span class="token punctuation">;</span>
+<span class="token comment">#error_log  logs/error.log;</span>
+<span class="token comment">#error_log  logs/error.log  notice;</span>
+<span class="token comment">#error_log  logs/error.log  info;</span>
+<span class="token comment">#pid        logs/nginx.pid;</span>
+events <span class="token punctuation">{</span>
+    worker_connections  <span class="token number">1024</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+ 
+ 
+http <span class="token punctuation">{</span>
+    include       mime.types<span class="token punctuation">;</span>
+    default_type  application/octet-stream<span class="token punctuation">;</span>
+    <span class="token comment">#access_log  logs/access.log  main;</span>
+    sendfile        on<span class="token punctuation">;</span>
+    <span class="token comment">#tcp_nopush     on;</span>
+    keepalive_timeout  <span class="token number">65</span><span class="token punctuation">;</span>
+    <span class="token comment">#gzip  on;</span>
+ 
+server <span class="token punctuation">{</span>
+        listen <span class="token number">88</span><span class="token punctuation">;</span>                <span class="token comment">#监听端口</span>
+        resolver <span class="token number">183.60</span>.82.98<span class="token punctuation">;</span>   <span class="token comment">#dns解析地址</span>
+        server_name  _<span class="token punctuation">;</span>
+        <span class="token comment">#charset koi8-r;</span>
+        <span class="token comment">#access_log  logs/host.access.log  main;</span>
+        location / <span class="token punctuation">{</span>
+             proxy_pass https://<span class="token variable">$host</span><span class="token variable">$request_uri</span><span class="token punctuation">;</span>     <span class="token comment">#设定http代理服务器的协议和地址</span>
+             proxy_set_header HOST <span class="token variable">$host</span><span class="token punctuation">;</span>
+             proxy_buffers <span class="token number">256</span> 4k<span class="token punctuation">;</span>
+             proxy_max_temp_file_size 0k<span class="token punctuation">;</span>
+             proxy_connect_timeout <span class="token number">30</span><span class="token punctuation">;</span>
+             proxy_send_timeout <span class="token number">60</span><span class="token punctuation">;</span>
+             proxy_read_timeout <span class="token number">60</span><span class="token punctuation">;</span>
+             proxy_next_upstream error <span class="token function">timeout</span> invalid_header http_502<span class="token punctuation">;</span>
+            <span class="token comment">#root   html;</span>
+            <span class="token comment">#index  index.html index.htm;</span>
+        <span class="token punctuation">}</span>
+        error_page   <span class="token number">500</span> <span class="token number">502</span> <span class="token number">503</span> <span class="token number">504</span>  /50x.html<span class="token punctuation">;</span>
+        location <span class="token operator">=</span> /50x.html <span class="token punctuation">{</span>
+            root   html<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+ 
+    <span class="token punctuation">}</span>
+ 
+ 
+ 
+server <span class="token punctuation">{</span>
+       resolver <span class="token number">8.8</span>.8.8<span class="token punctuation">;</span>   <span class="token comment">#dns解析地址</span>
+       listen <span class="token number">89</span><span class="token punctuation">;</span>          <span class="token comment">#代理监听端口</span>
+       proxy_connect<span class="token punctuation">;</span>
+       proxy_connect_allow            <span class="token number">443</span> <span class="token number">563</span><span class="token punctuation">;</span>
+       location / <span class="token punctuation">{</span>
+             proxy_pass https://<span class="token variable">$host</span><span class="token variable">$request_uri</span><span class="token punctuation">;</span>     <span class="token comment">#设定https代理服务器的协议和地址</span>
+             proxy_set_header HOST <span class="token variable">$host</span><span class="token punctuation">;</span>
+             proxy_buffers <span class="token number">256</span> 4k<span class="token punctuation">;</span>
+             proxy_max_temp_file_size 0k<span class="token punctuation">;</span>
+             proxy_connect_timeout <span class="token number">30</span><span class="token punctuation">;</span>
+             proxy_send_timeout <span class="token number">60</span><span class="token punctuation">;</span>
+             proxy_read_timeout <span class="token number">60</span><span class="token punctuation">;</span>
+             proxy_next_upstream error <span class="token function">timeout</span> invalid_header http_502<span class="token punctuation">;</span>
+ 
+       <span class="token punctuation">}</span>
+        error_page   <span class="token number">500</span> <span class="token number">502</span> <span class="token number">503</span> <span class="token number">504</span>  /50x.html<span class="token punctuation">;</span>
+        location <span class="token operator">=</span> /50x.html <span class="token punctuation">{</span>
+            root   html<span class="token punctuation">;</span>
+        <span class="token punctuation">}</span>
+ 
+    <span class="token punctuation">}</span>  
+ 
+<span class="token punctuation">}</span>　　
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="测试" tabindex="-1"><a class="header-anchor" href="#测试" aria-hidden="true">#</a> 测试</h2><div class="language-bash line-numbers-mode" data-ext="sh"><pre class="language-bash"><code><span class="token function">vim</span>  /etc/profile
+<span class="token punctuation">..</span>.
+<span class="token punctuation">..</span>.
+<span class="token comment">#export https_proxy=正向代理IP:端口</span>
+<span class="token builtin class-name">export</span> <span class="token assign-left variable">https_proxy</span><span class="token operator">=</span><span class="token number">192.168</span>.3.17:89
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),l=[t];function p(c,o){return s(),a("div",null,l)}const d=n(i,[["render",p],["__file","Nginx正向代理.html.vue"]]);export{d as default};
