ee8f6553a3572c6923fea5b063ed4f3445e32bcc
Paper-Dragon <2678885646@qq.com>
2024-01-15 02:05:39 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@2bd230701d3476d0102ecf038224b534354dc459 🚀

---
+++ assets/PowerShell 打印环境变量.html--FIQwHUS.js
@@ -0,0 +1,48 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,e}from"./app-iZmGDDCU.js";const i={},t=e(`<h1 id="powershell-打印环境变量" tabindex="-1"><a class="header-anchor" href="#powershell-打印环境变量" aria-hidden="true">#</a> PowerShell 打印环境变量</h1><div class="language-powershell line-numbers-mode" data-ext="powershell"><pre class="language-powershell"><code><span class="token function">PS</span> C:\\Users\\IT-Desktop&gt; <span class="token function">Get-ChildItem</span> <span class="token operator">-</span>Path Env:
+
+Name                           Value
+<span class="token operator">--</span><span class="token operator">--</span>                           <span class="token operator">--</span><span class="token operator">--</span><span class="token operator">-</span>
+ALLUSERSPROFILE                C:\\ProgramData
+APPDATA                        C:\\Users\\IT-Desktop\\AppData\\Roaming
+CLion                          C:\\Program Files\\JetBrains\\CLion 2022<span class="token punctuation">.</span>2\\bin<span class="token punctuation">;</span>
+CommonProgramFiles             C:\\Program Files\\Common Files
+CommonProgramFiles<span class="token punctuation">(</span>x86<span class="token punctuation">)</span>        C:\\Program Files <span class="token punctuation">(</span>x86<span class="token punctuation">)</span>\\Common Files
+CommonProgramW6432             C:\\Program Files\\Common Files
+COMPUTERNAME                   SHOULONG-DESK
+ComSpec                        C:\\Windows\\system32\\cmd<span class="token punctuation">.</span>exe
+DriverData                     C:\\Windows\\System32\\Drivers\\DriverData
+FPS_BROWSER_APP_PROFILE_STRING Internet Explorer
+FPS_BROWSER_USER_PROFILE_ST<span class="token punctuation">.</span><span class="token punctuation">.</span><span class="token punctuation">.</span> Default
+HOMEDRIVE                      C:
+HOMEPATH                       \\Users\\IT-Desktop
+LOCALAPPDATA                   C:\\Users\\IT-Desktop\\AppData\\Local
+LOGONSERVER                    \\\\SHOULONG-DESK
+NUMBER_OF_PROCESSORS           4
+OneDrive                       C:\\Users\\IT-Desktop\\OneDrive
+OS                             Windows_NT
+Path                           C:\\Program Files <span class="token punctuation">(</span>x86<span class="token punctuation">)</span>\\VMware\\VMware Workstation\\bin\\<span class="token punctuation">;</span>C:\\Program Files\\Python37\\Scrip<span class="token punctuation">.</span><span class="token punctuation">.</span><span class="token punctuation">.</span>
+PATHEXT                        <span class="token punctuation">.</span>COM<span class="token punctuation">;</span><span class="token punctuation">.</span>EXE<span class="token punctuation">;</span><span class="token punctuation">.</span>BAT<span class="token punctuation">;</span><span class="token punctuation">.</span>CMD<span class="token punctuation">;</span><span class="token punctuation">.</span>VBS<span class="token punctuation">;</span><span class="token punctuation">.</span>VBE<span class="token punctuation">;</span><span class="token punctuation">.</span>JS<span class="token punctuation">;</span><span class="token punctuation">.</span>JSE<span class="token punctuation">;</span><span class="token punctuation">.</span>WSF<span class="token punctuation">;</span><span class="token punctuation">.</span>WSH<span class="token punctuation">;</span><span class="token punctuation">.</span>MSC<span class="token punctuation">;</span><span class="token punctuation">.</span>PY<span class="token punctuation">;</span><span class="token punctuation">.</span>PYW<span class="token punctuation">;</span><span class="token punctuation">.</span>CPL
+PROCESSOR_ARCHITECTURE         AMD64
+PROCESSOR_IDENTIFIER           Intel64 Family 6 Model 158 Stepping 9<span class="token punctuation">,</span> GenuineIntel
+PROCESSOR_LEVEL                6
+PROCESSOR_REVISION             9e09
+ProgramData                    C:\\ProgramData
+ProgramFiles                   C:\\Program Files
+ProgramFiles<span class="token punctuation">(</span>x86<span class="token punctuation">)</span>              C:\\Program Files <span class="token punctuation">(</span>x86<span class="token punctuation">)</span>
+ProgramW6432                   C:\\Program Files
+PSModulePath                   C:\\Users\\IT-Desktop\\Documents\\WindowsPowerShell\\Modules<span class="token punctuation">;</span>C:\\Program Files\\WindowsPower<span class="token punctuation">.</span><span class="token punctuation">.</span><span class="token punctuation">.</span>
+PUBLIC                         C:\\Users\\Public
+PyCharm                        C:\\Program Files\\JetBrains\\PyCharm 2022<span class="token punctuation">.</span>2<span class="token punctuation">.</span>2\\bin<span class="token punctuation">;</span>
+SESSIONNAME                    Console
+SystemDrive                    C:
+SystemRoot                     C:\\Windows
+TEMP                           C:\\Users\\IT-DES~1\\AppData\\Local\\Temp
+TMP                            C:\\Users\\IT-DES~1\\AppData\\Local\\Temp
+USERDOMAIN                     SHOULONG-DESK
+USERDOMAIN_ROAMINGPROFILE      SHOULONG-DESK
+USERNAME                       IT-Desktop
+USERPROFILE                    C:\\Users\\IT-Desktop
+windir                         C:\\Windows
+ZES_ENABLE_SYSMAN              1
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,2),o=[t];function l(p,c){return s(),a("div",null,o)}const d=n(i,[["render",l],["__file","PowerShell 打印环境变量.html.vue"]]);export{d as default};
