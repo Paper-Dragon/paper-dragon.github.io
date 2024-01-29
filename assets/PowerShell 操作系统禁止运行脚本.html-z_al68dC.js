66f283f7f60a0bb0cb0598fd63802a6025555268
Paper-Dragon <2678885646@qq.com>
2024-01-29 10:49:55 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@041296aa40977e0326bc6ebdeb0272c4ecd32b4b 🚀

---
+++ assets/PowerShell 操作系统禁止运行脚本.html-z_al68dC.js
@@ -0,0 +1,6 @@
+import{_ as o}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as l,c as n,b as e}from"./app-iXloerPV.js";const t={},_=e("h1",{id:"powershell-操作系统禁止运行脚本",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#powershell-操作系统禁止运行脚本"},[e("span",null,"PowerShell 操作系统禁止运行脚本")])],-1),c=e("blockquote",null,[e("p",null,'在使用 VS code 自带终端的时会报出"系统禁止脚本运行的错误"，原因是因为 PowerShell 执行策略的问题。')],-1),s=e("h2",{id:"解决方法",tabindex:"-1"},[e("a",{class:"header-anchor",href:"#解决方法"},[e("span",null,"解决方法：")])],-1),r=e("pre",null,[e("code",null,`管理员身份运行 PowerShell
+执行：get-ExecutionPolicy，若显示 Restricted 表示状态是禁止的
+执行：set-ExecutionPolicy，会提示输入参数
+输入 RemoteSigned 会提示进行选择
+输入：Y，回车
+`)],-1),a=[_,c,s,r];function h(i,d){return l(),n("div",null,a)}const f=o(t,[["render",h],["__file","PowerShell 操作系统禁止运行脚本.html.vue"]]);export{f as default};
---
+++ assets/使用curl实现邮件发送.html-VS6rfcn5.js
@@ -0,0 +1,28 @@
+import{_ as a}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as n,d as e}from"./app-iXloerPV.js";const i="/assets/image-20220810164720943-vAB5LtfF.png",l="/assets/image-20220810164713378-qsgzFnME.png",t={},r=e(`<h1 id="使用curl实现邮件发送" tabindex="-1"><a class="header-anchor" href="#使用curl实现邮件发送"><span>使用curl实现邮件发送</span></a></h1><h1 id="概述" tabindex="-1"><a class="header-anchor" href="#概述"><span>概述</span></a></h1><p>本文讲解如何通过curl实现邮件发送功能，通过此功能，可以将一些配置信息自动发送到指定邮箱，满足自动运维要求。 测试发件邮箱：sina 测试收件邮箱：qq</p><h1 id="脚本实例" tabindex="-1"><a class="header-anchor" href="#脚本实例"><span>脚本实例</span></a></h1><p>curl_send_mail.bat</p><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code>@echo off
+rem 使用curl实现邮件发送
+rem -------------------------------------------
+rem 参数
+<span class="token builtin class-name">set</span> <span class="token assign-left variable">smtp</span><span class="token operator">=</span><span class="token string">&quot;smtp://smtp.sina.com&quot;</span>
+<span class="token builtin class-name">set</span> <span class="token assign-left variable">mail_from</span><span class="token operator">=</span>发件地址@sina.com
+<span class="token builtin class-name">set</span> <span class="token assign-left variable">mail_to</span><span class="token operator">=</span>收件地址@sina.com
+<span class="token builtin class-name">set</span> <span class="token assign-left variable">data</span><span class="token operator">=</span>mail.txt
+<span class="token builtin class-name">set</span> <span class="token assign-left variable">user</span><span class="token operator">=</span><span class="token string">&quot;发件人用户名:发件人密码&quot;</span>
+rem -------------------------------------------
+rem 编写邮件头消息
+<span class="token builtin class-name">echo</span> From:%mail_from% <span class="token operator">&gt;</span> %data%
+<span class="token builtin class-name">echo</span> To:%mail_to% <span class="token operator">&gt;&gt;</span> %data%
+<span class="token builtin class-name">echo</span> Subject:邮件内容标题 %date%<span class="token operator">&gt;&gt;</span> %data%
+echo.<span class="token operator">&gt;&gt;</span> %data%
+
+<span class="token builtin class-name">echo</span> 这里填写具体的邮件内容 <span class="token operator">&gt;&gt;</span> %data%
+rem 例如：获取当前的外网地址信息
+rem <span class="token function">curl</span> ifconfig.me <span class="token operator">&gt;&gt;</span> %data%
+rem -------------------------------------------
+rem 发送邮件
+<span class="token function">curl</span> <span class="token parameter variable">--verbose</span> <span class="token parameter variable">-s</span> <span class="token parameter variable">--url</span> <span class="token string">&quot;%smtp%&quot;</span> --mail-from %mail_from% --mail-rcpt %mail_to% --upload-file %data% <span class="token parameter variable">--user</span> %user%
+
+rem 删除临时文件
+del %data%
+pause
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h1 id="执行结果" tabindex="-1"><a class="header-anchor" href="#执行结果"><span>执行结果</span></a></h1><figure><img src="`+i+'" alt="image-20220810164720943" tabindex="0" loading="lazy"><figcaption>image-20220810164720943</figcaption></figure><h1 id="邮箱配置" tabindex="-1"><a class="header-anchor" href="#邮箱配置"><span>邮箱配置</span></a></h1><p>新浪邮箱配置： <img src="'+l+'" alt="image-20220810164713378" loading="lazy"></p>',10),c=[r];function o(p,d){return s(),n("div",null,c)}const v=a(t,[["render",o],["__file","使用curl实现邮件发送.html.vue"]]);export{v as default};
