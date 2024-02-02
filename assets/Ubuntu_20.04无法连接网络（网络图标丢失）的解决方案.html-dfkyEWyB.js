67147e1b8949004564e9abe6c22758597f6c519a
Paper-Dragon <2678885646@qq.com>
2024-02-02 03:32:02 +0000

    Deploying to geekery_cn-pages from @ Paper-Dragon/paper-dragon.github.io@e626c79df3e083d2c7dc676ff94b6ff01d34ed4a 🚀

--- assets/Ubuntu_20.04无法连接网络（网络图标丢失）的解决方案.html-dfkyEWyB.js
+++
@@ -1,8 +0,0 @@
-import{_ as e}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as n,d as t}from"./app-C9BS2DyX.js";const s="/assets/image-20221124112138202-ei62Lezn.png",r="/assets/image-20221124112145118-rj2u4eN-.png",o={},c=t('<h1 id="ubuntu-20-04无法连接网络-网络图标丢失-的解决方案" tabindex="-1"><a class="header-anchor" href="#ubuntu-20-04无法连接网络-网络图标丢失-的解决方案"><span>Ubuntu 20.04无法连接网络(网络图标丢失)的解决方案</span></a></h1><blockquote><p>from csdn: 罗伯特祥</p></blockquote><h2 id="问题复述" tabindex="-1"><a class="header-anchor" href="#问题复述"><span>问题复述：</span></a></h2><p>Ubuntu 20.04无法连接到网络，网络连接图标丢失，网络设置中无网络设置选项。 <img src="'+s+`" alt="image-20221124112138202" loading="lazy"></p><h2 id="解决方案" tabindex="-1"><a class="header-anchor" href="#解决方案"><span>解决方案</span></a></h2><pre><code>sudo service network-manager stop
-
-sudo rm /var/lib/NetworkManager/NetworkManager.state
-
-sudo service network-manager start
-</code></pre><figure><img src="`+r+`" alt="image-20221124112145118" tabindex="0" loading="lazy"><figcaption>image-20221124112145118</figcaption></figure><h2 id="参考文献" tabindex="-1"><a class="header-anchor" href="#参考文献"><span>参考文献：</span></a></h2><pre><code>https://www.cnblogs.com/shiyueyangne/p/14260820.html
-https://www.freesion.com/article/3238793033/
-</code></pre>`,9),i=[c];function _(d,p){return a(),n("div",null,i)}const l=e(o,[["render",_],["__file","Ubuntu_20.04无法连接网络（网络图标丢失）的解决方案.html.vue"]]);export{l as default};
---
+++ assets/匹配特定字符并输出其后的若干行.html-zJMkphLK.js
@@ -0,0 +1,21 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,d as e}from"./app-23B57rN0.js";const i={},o=e(`<h1 id="awk-输出配行及匹配下面的三行" tabindex="-1"><a class="header-anchor" href="#awk-输出配行及匹配下面的三行"><span>awk 输出配行及匹配下面的三行</span></a></h1><h2 id="用法" tabindex="-1"><a class="header-anchor" href="#用法"><span>用法：</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">awk</span> <span class="token string">&#39;/xxoo/{p=4}p--&gt;0&#39;</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="实践" tabindex="-1"><a class="header-anchor" href="#实践"><span>实践</span></a></h2><h3 id="示例" tabindex="-1"><a class="header-anchor" href="#示例"><span>示例：</span></a></h3><pre><code># cat /etc/passwd
+
+root:x:0:0:root:/root:/bin/bash
+bin:x:1:1:bin:/bin:/sbin/nologin
+daemon:x:2:2:daemon:/sbin:/sbin/nologin
+adm:x:3:4:adm:/var/adm:/sbin/nologin
+lp:x:4:7:lp:/var/spool/lpd:/sbin/nologin
+sync:x:5:0:sync:/sbin:/bin/sync
+shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
+halt:x:7:0:halt:/sbin:/sbin/halt
+mail:x:8:12:mail:/var/spool/mail:/sbin/nologin
+operator:x:11:0:operator:/root:/sbin/nologin
+games:x:12:100:games:/usr/games:/sbin/nologin
+</code></pre><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token comment">#实现指令</span>
+
+<span class="token comment"># cat /etc/passwd|awk &#39;/sync/{p=4}p--&gt;0&#39;</span>
+sync:x:5:0:sync:/sbin:/bin/sync
+shutdown:x:6:0:shutdown:/sbin:/sbin/shutdown
+halt:x:7:0:halt:/sbin:/sbin
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,7),d=[o];function t(l,c){return s(),a("div",null,d)}const b=n(i,[["render",t],["__file","匹配特定字符并输出其后的若干行.html.vue"]]);export{b as default};
