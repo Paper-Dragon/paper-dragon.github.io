6df710888ff27d1bd6fb80ac8dd849527f954a77
Paper-Dragon <2678885646@qq.com>
2024-02-02 16:33:57 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@cf6c0ec8504b01ffbc321ddb9479c97bbe7333f8 🚀

--- assets/shell脚本bad substitution.html-UJbsxM94.js
+++
@@ -1,5 +0,0 @@
-import{_ as s}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as n,c as e,b as t}from"./app-EC-IP3MR.js";const o={},i=t("h1",{id:"bad-substitution",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#bad-substitution"},[t("span",null,"bad substitution")])],-1),l=t("p",null,"初接触shell脚本，在vim中写代码，出现了好几次 Bad substitution。",-1),a=t("p",null,"我的错误有两种：",-1),_=t("pre",null,[t("code",null,`开始的的指定脚本环境 应该是#!/bin/bash
-在编译运行时 也应该用 bash
-\${}的使用错误，$() 是引用（）中运行的结果。
-\${} 仅仅是用{}中的内容，是参数，不执行
-`)],-1),c=[i,l,a,_];function u(d,r){return n(),e("div",null,c)}const f=s(o,[["render",u],["__file","shell脚本bad substitution.html.vue"]]);export{f as default};
