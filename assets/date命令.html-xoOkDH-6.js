9684ce5339b5cfbb5b5edf5deb5711c10554ee9e
Paper-Dragon <2678885646@qq.com>
2024-02-06 08:00:18 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@1a4ecc308675a95109cf554f9e8ab67a833aa0ae 🚀

---
+++ assets/date命令.html-xoOkDH-6.js
@@ -0,0 +1,33 @@
+import{_ as o}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as d,c as e,d as t}from"./app-d-G7c0BN.js";const n={},a=t(`<h1 id="date-显示或设置时间" tabindex="-1"><a class="header-anchor" href="#date-显示或设置时间"><span>date 显示或设置时间</span></a></h1><h2 id="显示时间" tabindex="-1"><a class="header-anchor" href="#显示时间"><span>显示时间</span></a></h2><p>按照我们要求的格式显示当前日期: 年-月-日</p><pre><code>2019-04-10
+[root@oldboyedu59 ~]# date  +%Y-%m-%d
+2019-04-10
+[root@oldboyedu59 ~]# date +%T
+09:57:23
+[root@oldboyedu59 ~]# date +%H:%M:%S
+09:57:39
+[root@oldboyedu59 ~]# date +%w
+3
+ 
++%F === +%Y-%m-%d
+          year month  day 
++%T === +%H:%M:%S
+          hour min(minute)  sec(second)       
++%w === week 周几 
+</code></pre><h2 id="例题" tabindex="-1"><a class="header-anchor" href="#例题"><span>例题:</span></a></h2><p>显示当前时间以年月日-小时格式 20190101-10 显示当前时间以年月日-小时_周几 格式 2019_01_01-10_3</p><pre><code>[root@oldboyedu59 ~]# #显示当前时间以年月日-小时格式
+[root@oldboyedu59 ~]# date +%Y%m%d
+20190410
+[root@oldboyedu59 ~]# date +%Y%m%d-%H
+20190410-10
+[root@oldboyedu59 ~]# #显示当前时间以年_月_日-小时_周几 格式
+[root@oldboyedu59 ~]# date +%Y_%m_%d-%H_%w
+2019_04_10-10_3
+</code></pre><p>​<br> [root@oldboyedu59 ~]# date -d &quot;10day&quot; Sat Apr 20 10:10:12 CST 2019 [root@oldboyedu59 ~]# date -d &quot;10day&quot; +%F 2019-04-20</p><p>显示1天前时间以年月日-周几格式 20190101-3 显示7天前时间 以年月日-小时_周几 格式 2019_01_01-10_3</p><pre><code>[root@oldboyedu59 ~]# #显示1天前时间以年月日-周几格式
+[root@oldboyedu59 ~]# 
+[root@oldboyedu59 ~]# 
+[root@oldboyedu59 ~]# date +%Y%m%d-%w -d  &quot;-1day&quot;
+20190409-2
+[root@oldboyedu59 ~]# #显示7天前时间 以年_月_日-小时_周几 格式
+[root@oldboyedu59 ~]# 
+[root@oldboyedu59 ~]# date +%Y_%m_%d-%H_%w -d &#39;-7day&#39;
+2019_04_03-10_3
+</code></pre><p>备份 设置时间</p><p>set date -s</p>`,12),r=[a];function _(c,u){return d(),e("div",null,r)}const s=o(n,[["render",_],["__file","date命令.html.vue"]]);export{s as default};
---
+++ assets/制作CenOS操作系统.html-B2LrFrUA.js
@@ -0,0 +1,46 @@
+import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as s,c as a,d as e}from"./app-d-G7c0BN.js";const t={},o=e(`<h1 id="制作centos操作系统rootfs" tabindex="-1"><a class="header-anchor" href="#制作centos操作系统rootfs"><span>制作CentOS操作系统RootFS</span></a></h1><h2 id="基础安装" tabindex="-1"><a class="header-anchor" href="#基础安装"><span>基础安装</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token punctuation">[</span>root@monther ~<span class="token punctuation">]</span><span class="token comment"># mkdir mkRootfs</span>
+<span class="token punctuation">[</span>root@monther ~<span class="token punctuation">]</span><span class="token comment">#</span>
+<span class="token punctuation">[</span>root@monther ~<span class="token punctuation">]</span><span class="token comment"># cd mkRootfs/</span>
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># mount /dev/sr0 /root/mkRootfs/iso/</span>
+mount: /dev/sr0 is write-protected, mounting read-only
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># ls iso/</span>
+CentOS_BuildTag  EFI  EULA  GPL  images  isolinux  LiveOS  Packages  repodata  RPM-GPG-KEY-CentOS-7  RPM-GPG-KEY-CentOS-Testing-7  TRANS.TBL
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># mkdir rootfs</span>
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># ls</span>
+iso  rootfs
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># cp iso/Packages/centos-release-7-7.1908.0.el7.centos.x86_64.rpm .</span>
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># ls</span>
+centos-release-7-7.1908.0.el7.centos.x86_64.rpm  iso  rootfs
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># rpm --root=/root/mkRootfs/rootfs/ -ivh --nodeps /root/mkRootfs/centos-release-7-7.1908.0.el7.centos.x86_64.rpm</span>
+warning: /root/mkRootfs/centos-release-7-7.1908.0.el7.centos.x86_64.rpm: Header V3 RSA/SHA256 Signature, key ID f4a80eb5: NOKEY
+Preparing<span class="token punctuation">..</span>.                          <span class="token comment">################################# [100%]</span>
+Updating / installing<span class="token punctuation">..</span>.
+   <span class="token number">1</span>:centos-release-7-7.1908.0.el7.cen<span class="token comment">################################# [100%]</span>
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># ls rootfs/</span>
+etc  usr  var
+<span class="token punctuation">[</span>root@monther mkRootfs<span class="token punctuation">]</span><span class="token comment"># yum -y --installroot=/root/mkRootfs/rootfs/ install basesystem filesystem bash kernel passwd @base @core</span>
+Loaded plugins: fastestmirror
+Determining fastest mirrors
+ * base: mirrors.aliyun.com
+ * extras: mirrors.aliyun.com
+ * updates: mirrors.aliyun.com
+base                                                                                                                                                 <span class="token operator">|</span> <span class="token number">3.6</span> kB  00:00:00
+extras                                                                                                                                               <span class="token operator">|</span> <span class="token number">2.9</span> kB  00:00:00
+updates                                                                                                                                              <span class="token operator">|</span> <span class="token number">2.9</span> kB  00:00:00
+<span class="token punctuation">(</span><span class="token number">1</span>/4<span class="token punctuation">)</span>: base/7/x86_64/primary_db                                                                                                                      <span class="token operator">|</span> <span class="token number">6.1</span> MB  00:00:00
+<span class="token punctuation">(</span><span class="token number">2</span>/4<span class="token punctuation">)</span>: base/7/x86_64/group_gz                                                                                                                        <span class="token operator">|</span> <span class="token number">153</span> kB  00:00:01
+<span class="token punctuation">(</span><span class="token number">3</span>/4<span class="token punctuation">)</span>: extras/7/x86_64/primary_db                                                                                                                    <span class="token operator">|</span> <span class="token number">250</span> kB  00:00:01
+<span class="token punctuation">(</span><span class="token number">4</span>/4<span class="token punctuation">)</span>: updates/7/x86_64/primary_db                                                                                                                   <span class="token operator">|</span>  <span class="token number">17</span> MB  00:01:56
+                                           <span class="token operator">|</span>  <span class="token number">17</span> MB  00:01:56
+Resolving Dependencies
+--<span class="token operator">&gt;</span> Running transaction check
+-<span class="token operator">&gt;</span> Processing Dependency: libglib-2.0.so.0<span class="token punctuation">(</span><span class="token punctuation">)</span><span class="token punctuation">(</span>64bit<span class="token punctuation">)</span> <span class="token keyword">for</span> package: <span class="token number">1</span>:NetworkManager-1.18.8-2.el7_9.x86_64
+<span class="token punctuation">..</span><span class="token punctuation">..</span>.
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h2 id="初始化" tabindex="-1"><a class="header-anchor" href="#初始化"><span>初始化</span></a></h2><blockquote><p>手动创建用户初始化程序init的链接，并设置用于解压到Client上的CentOS的口令。</p></blockquote><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">tar</span> <span class="token parameter variable">-cvf</span> <span class="token number">11</span>.tar <span class="token builtin class-name">.</span>
+<span class="token function">docker</span> <span class="token function">import</span> <span class="token number">11</span>.tar centos:custom
+<span class="token punctuation">[</span>root@monther rootfs<span class="token punctuation">]</span><span class="token comment"># docker run -it --rm --privileged centos:custom init</span>
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,6),i=[o];function p(c,l){return s(),a("div",null,i)}const m=n(t,[["render",p],["__file","制作CenOS操作系统.html.vue"]]);export{m as default};
