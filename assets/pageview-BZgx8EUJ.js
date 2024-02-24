2b7d92f1f59530bfcc773e710fa90d600f2b7f47
Paper-Dragon <2678885646@qq.com>
2024-02-24 11:05:49 +0000

    Deploying to geekery_cn-pages from @ Paper-Dragon/paper-dragon.github.io@e0ada2593250f9a070daf469eff121aed3d0596f 🚀

--- assets/pageview-BZgx8EUJ.js
+++
@@ -1,7 +0,0 @@
-import{a7 as r}from"./app-DZTjsCUu.js";var a={provider:"Waline",dark:'html[data-theme="dark"]',serverURL:"https://comment.geekery.cn/"};const o=async()=>{try{const{pageviewCount:e}=await r(()=>import("./app-DZTjsCUu.js").then(t=>t.af),__vite__mapDeps([]));return e({serverURL:a.serverURL})}catch{console.error("@waline/client is not installed!")}};export{o as updatePageview};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
