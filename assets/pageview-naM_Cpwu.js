64ac53761773d2a0fedce8edce9abad1b5595df6
Paper-Dragon <2678885646@qq.com>
2024-03-20 09:28:58 +0000

    Deploying to geekery_cn-pages from @ Paper-Dragon/paper-dragon.github.io@026b6a25c2d48b2cefb5ad011d58cad353aaa13c 🚀

--- assets/pageview-naM_Cpwu.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = []
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{_ as o,a8 as n}from"./app-Cq2c9HnX.js";const i=async()=>{try{const{pageviewCount:e}=await o(()=>import("./app-Cq2c9HnX.js").then(r=>r.ah),__vite__mapDeps([])),t=n();return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
