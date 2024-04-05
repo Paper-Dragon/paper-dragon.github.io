18f286139fc39b0066751af899904f1b88e2d3bb
Paper-Dragon <2678885646@qq.com>
2024-04-05 13:05:07 +0000

    Deploying to gh-pages from @ Paper-Dragon/paper-dragon.github.io@7d0258b170bd81c3607906f5101e6ad98b7e8484 🚀

--- assets/pageview-Dt-U8f94.js
+++
@@ -1,1 +0,0 @@
-import{a8 as o,_ as n}from"./app-DjHPN4cX.js";const i=async()=>{const t=o();try{const{pageviewCount:e}=await n(()=>import("./app-DjHPN4cX.js").then(r=>r.ah),[]);return e({serverURL:t.serverURL})}catch{console.error("@waline/client is not installed!")}};export{i as updatePageview};
