39a0ef93e6330fc9fb8e7ac6e72e43615d6a5a56
Paper-Dragon <2678885646@qq.com>
2024-05-25 07:34:59 +0000

    Deploying to geekery_cn-pages from @ Paper-Dragon/paper-dragon.github.io@db07b0b15387a93ae5f284ff37212804d9409390 🚀

---
+++ assets/jenkins的docker部署文档.html-ERd_M-aQ.js
@@ -0,0 +1,77 @@
+import{_ as n}from"./plugin-vue_export-helper-DlAUqK2U.js";import{c as s,o as e,d as a}from"./app-DYXE0zJL.js";const t={},i=a(`<h1 id="jenkins的docker部署文档" tabindex="-1"><a class="header-anchor" href="#jenkins的docker部署文档"><span>jenkins的docker部署文档</span></a></h1><h2 id="起一个-jenkins" tabindex="-1"><a class="header-anchor" href="#起一个-jenkins"><span>起一个 jenkins</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code><span class="token function">docker</span> run  <span class="token parameter variable">-u</span> root  <span class="token parameter variable">-it</span>   <span class="token parameter variable">-d</span>  <span class="token parameter variable">-p</span> <span class="token number">8080</span>:8080 <span class="token parameter variable">-p</span> <span class="token number">50000</span>:50000 <span class="token parameter variable">-v</span> jenkins-data:/var/jenkins_home <span class="token parameter variable">-v</span> /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div></div></div><h2 id="配置docker-api接口" tabindex="-1"><a class="header-anchor" href="#配置docker-api接口"><span>配置docker api接口</span></a></h2><div class="language-bash line-numbers-mode" data-ext="sh" data-title="sh"><pre class="language-bash"><code>
+root@shoulong-jenkins-server:~/jenkins-data<span class="token comment"># cat /etc/default/docker</span>
+<span class="token comment"># Docker Upstart and SysVinit configuration file</span>
+
+<span class="token comment">#</span>
+<span class="token comment"># THIS FILE DOES NOT APPLY TO SYSTEMD</span>
+<span class="token comment">#</span>
+<span class="token comment">#   Please see the documentation for &quot;systemd drop-ins&quot;:</span>
+<span class="token comment">#   https://docs.docker.com/engine/admin/systemd/</span>
+<span class="token comment">#</span>
+
+<span class="token comment"># Customize location of Docker binary (especially for development testing).</span>
+<span class="token comment">#DOCKERD=&quot;/usr/local/bin/dockerd&quot;</span>
+
+<span class="token comment"># Use DOCKER_OPTS to modify the daemon startup options.</span>
+<span class="token comment">#DOCKER_OPTS=&quot;--dns 8.8.8.8 --dns 8.8.4.4&quot;</span>
+
+<span class="token comment"># If you need Docker to use an HTTP proxy, it can also be specified here.</span>
+<span class="token comment">#export http_proxy=&quot;http://127.0.0.1:3128/&quot;</span>
+
+<span class="token comment"># This is also a handy place to tweak where Docker&#39;s temporary files go.</span>
+<span class="token comment">#export DOCKER_TMPDIR=&quot;/mnt/bigdrive/docker-tmp&quot;</span>
+<span class="token assign-left variable">DOCKER_OPTS</span><span class="token operator">=</span><span class="token string">&#39;-H tcp://0.0.0.0:2376 -H unix:///var/run/docker.sock&#39;</span>
+
+root@shoulong-jenkins-server:~/jenkins-data<span class="token comment"># cat /lib/systemd/system/docker.service</span>
+<span class="token punctuation">[</span>Unit<span class="token punctuation">]</span>
+<span class="token assign-left variable">Description</span><span class="token operator">=</span>Docker Application Container Engine
+<span class="token assign-left variable">Documentation</span><span class="token operator">=</span>https://docs.docker.com
+<span class="token assign-left variable">After</span><span class="token operator">=</span>network-online.target firewalld.service containerd.service
+<span class="token assign-left variable">Wants</span><span class="token operator">=</span>network-online.target
+<span class="token assign-left variable">Requires</span><span class="token operator">=</span>docker.socket containerd.service
+
+<span class="token punctuation">[</span>Service<span class="token punctuation">]</span>
+<span class="token assign-left variable">Type</span><span class="token operator">=</span>notify
+<span class="token comment"># the default is not to use systemd for cgroups because the delegate issues still</span>
+<span class="token comment"># exists and systemd currently does not support the cgroup feature set required</span>
+<span class="token comment"># for containers run by docker</span>
+<span class="token assign-left variable">EnvironmentFile</span><span class="token operator">=</span>-/etc/default/docker
+<span class="token assign-left variable">ExecStart</span><span class="token operator">=</span>/usr/bin/dockerd <span class="token parameter variable">-H</span> fd:// <span class="token variable">$DOCKER_OPTS</span> <span class="token parameter variable">--containerd</span><span class="token operator">=</span>/run/containerd/containerd.sock
+<span class="token assign-left variable">ExecReload</span><span class="token operator">=</span>/bin/kill <span class="token parameter variable">-s</span> HUP <span class="token variable">$MAINPID</span>
+<span class="token assign-left variable">TimeoutSec</span><span class="token operator">=</span><span class="token number">0</span>
+<span class="token assign-left variable">RestartSec</span><span class="token operator">=</span><span class="token number">2</span>
+<span class="token assign-left variable">Restart</span><span class="token operator">=</span>always
+
+<span class="token comment"># Note that StartLimit* options were moved from &quot;Service&quot; to &quot;Unit&quot; in systemd 229.</span>
+<span class="token comment"># Both the old, and new location are accepted by systemd 229 and up, so using the old location</span>
+<span class="token comment"># to make them work for either version of systemd.</span>
+<span class="token assign-left variable">StartLimitBurst</span><span class="token operator">=</span><span class="token number">3</span>
+
+<span class="token comment"># Note that StartLimitInterval was renamed to StartLimitIntervalSec in systemd 230.</span>
+<span class="token comment"># Both the old, and new name are accepted by systemd 230 and up, so using the old name to make</span>
+<span class="token comment"># this option work for either version of systemd.</span>
+<span class="token assign-left variable">StartLimitInterval</span><span class="token operator">=</span>60s
+
+<span class="token comment"># Having non-zero Limit*s causes performance problems due to accounting overhead</span>
+<span class="token comment"># in the kernel. We recommend using cgroups to do container-local accounting.</span>
+<span class="token assign-left variable">LimitNOFILE</span><span class="token operator">=</span>infinity
+<span class="token assign-left variable">LimitNPROC</span><span class="token operator">=</span>infinity
+<span class="token assign-left variable">LimitCORE</span><span class="token operator">=</span>infinity
+
+<span class="token comment"># Comment TasksMax if your systemd version does not support it.</span>
+<span class="token comment"># Only systemd 226 and above support this option.</span>
+<span class="token assign-left variable">TasksMax</span><span class="token operator">=</span>infinity
+
+<span class="token comment"># set delegate yes so that systemd does not reset the cgroups of docker containers</span>
+<span class="token assign-left variable">Delegate</span><span class="token operator">=</span>yes
+
+<span class="token comment"># kill only the docker process, not all processes in the cgroup</span>
+<span class="token assign-left variable">KillMode</span><span class="token operator">=</span>process
+<span class="token assign-left variable">OOMScoreAdjust</span><span class="token operator">=</span>-500
+
+<span class="token punctuation">[</span>Install<span class="token punctuation">]</span>
+<span class="token assign-left variable">WantedBy</span><span class="token operator">=</span>multi-user.target
+
+
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),o=[i];function r(l,c){return e(),s("div",null,o)}const m=n(t,[["render",r],["__file","jenkins的docker部署文档.html.vue"]]),v=JSON.parse('{"path":"/note-book/Jenkins/jenkins%E7%9A%84docker%E9%83%A8%E7%BD%B2%E6%96%87%E6%A1%A3.html","title":"jenkins的docker部署文档","lang":"zh-CN","frontmatter":{"description":"jenkins的docker部署文档 起一个 jenkins 配置docker api接口","head":[["meta",{"property":"og:url","content":"https://www.geekery.cn/note-book/Jenkins/jenkins%E7%9A%84docker%E9%83%A8%E7%BD%B2%E6%96%87%E6%A1%A3.html"}],["meta",{"property":"og:site_name","content":"运维开发绿皮书"}],["meta",{"property":"og:title","content":"jenkins的docker部署文档"}],["meta",{"property":"og:description","content":"jenkins的docker部署文档 起一个 jenkins 配置docker api接口"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2024-03-08T05:39:39.000Z"}],["meta",{"property":"article:author","content":"PaperDragon"}],["meta",{"property":"article:modified_time","content":"2024-03-08T05:39:39.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"jenkins的docker部署文档\\",\\"image\\":[\\"\\"],\\"dateModified\\":\\"2024-03-08T05:39:39.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"PaperDragon\\",\\"url\\":\\"https://github.com/Paper-Dragon\\",\\"email\\":\\"2678885646@qq.com\\"}]}"]]},"headers":[{"level":2,"title":"起一个 jenkins","slug":"起一个-jenkins","link":"#起一个-jenkins","children":[]},{"level":2,"title":"配置docker api接口","slug":"配置docker-api接口","link":"#配置docker-api接口","children":[]}],"git":{"createdTime":1691939318000,"updatedTime":1709876379000,"contributors":[{"name":"PaperDragon","email":"2678885646@qq.com","commits":1},{"name":"PaperDragon-SH","email":"2678885646@qq.com","commits":1}]},"readingTime":{"minutes":1.3,"words":389},"filePathRelative":"note-book/Jenkins/jenkins的docker部署文档.md","localizedDate":"2023年8月13日","excerpt":"\\n<h2>起一个 jenkins</h2>\\n<div class=\\"language-bash\\" data-ext=\\"sh\\" data-title=\\"sh\\"><pre class=\\"language-bash\\"><code><span class=\\"token function\\">docker</span> run  <span class=\\"token parameter variable\\">-u</span> root  <span class=\\"token parameter variable\\">-it</span>   <span class=\\"token parameter variable\\">-d</span>  <span class=\\"token parameter variable\\">-p</span> <span class=\\"token number\\">8080</span>:8080 <span class=\\"token parameter variable\\">-p</span> <span class=\\"token number\\">50000</span>:50000 <span class=\\"token parameter variable\\">-v</span> jenkins-data:/var/jenkins_home <span class=\\"token parameter variable\\">-v</span> /var/run/docker.sock:/var/run/docker.sock jenkinsci/blueocean\\n</code></pre></div>","autoDesc":true}');export{m as comp,v as data};
